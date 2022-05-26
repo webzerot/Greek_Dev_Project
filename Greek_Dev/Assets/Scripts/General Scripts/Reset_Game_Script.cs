e3786212641a9837be101247f8941737e62d8403
webzerot <webzerot@gmail.com>
2022-05-26 21:02:44 +0300

    Reset_Game_Script

---
+++ Greek_Dev/Assets/Scripts/General Scripts/Reset_Game_Script.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Reset_Game_Script : MonoBehaviour
+{
+
+    [SerializeField] bool RestartWithR = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            ResetScene();
+        }
+    }
+
+    public void ResetScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
