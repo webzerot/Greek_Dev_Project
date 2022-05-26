89d7f427ecd73456f7970766080503901717e39e
webzerot <webzerot@gmail.com>
2022-05-26 20:50:33 +0300

    Scene_Changer_Script

---
+++ Greek_Dev/Assets/Scripts/General Scripts/Scene_Changer_Script.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene_Changer_Script : MonoBehaviour
+{
+    public void ChangeSceneOnBtn(string SceneName)
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+}
