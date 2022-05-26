03bb074bf4919912f6cf97ec18158680631bd119
webzerot <webzerot@gmail.com>
2022-05-26 21:04:36 +0300

    Test

--- Greek_Dev/Assets/Scripts/General Scripts/Reset_Game_Script.cs
+++ Greek_Dev/Assets/Scripts/General Scripts/Reset_Game_Script.cs
@@ -11,7 +11,7 @@ public class Reset_Game_Script : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.R))
+        if ((Input.GetKeyDown(KeyCode.R)) && (RestartWithR))
         {
             ResetScene();
         }
