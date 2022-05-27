05d71d0211cb864dd79989f2cfe6f204aaa25e4f
webzerot <webzerot@gmail.com>
2022-05-27 18:19:55 +0300

    Platform

--- Greek_Dev/Assets/Scripts/Platform.cs
+++ Greek_Dev/Assets/Scripts/Platform.cs
@@ -9,11 +9,13 @@ public class Platform : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-    }
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        rb.velocity += Vector2.down * speed;
 
+    }
     // Update is called once per frame
     void Update()
     {
-        transform.position -= new Vector3(0, speed * Time.deltaTime);
+        //transform.position -= new Vector3(0, speed * Time.deltaTime);
     }
 }
