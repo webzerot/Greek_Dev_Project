ab2d83751e0f31edb8409f06ee1a0e5152bf6d01
webzerot <webzerot@gmail.com>
2022-05-27 18:12:40 +0300

    Update

--- Greek_Dev/Assets/Scripts/PlayerMovement.cs
+++ Greek_Dev/Assets/Scripts/PlayerMovement.cs
@@ -7,12 +7,6 @@ public class PlayerMovement : MonoBehaviour
 
     [SerializeField] float PlayerSpeed;
     [SerializeField] float minX, maxX;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
