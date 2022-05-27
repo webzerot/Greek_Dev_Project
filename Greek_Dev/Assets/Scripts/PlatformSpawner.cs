c301d82580a3e9fd6dfccbdf1a5bda7999bb3af9
webzerot <webzerot@gmail.com>
2022-05-27 18:07:10 +0300

    Upd

---
+++ Greek_Dev/Assets/Scripts/PlatformSpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformSpawner : MonoBehaviour
+{
+
+    [SerializeField] GameObject Platform;
+    [SerializeField] float minX, maxX;
+    [SerializeField] float yPos;
+    private float time = 0.0f;
+    [SerializeField] float interpolationPeriod = 0.1f;
+    // Update is called once per frame
+    void Update()
+    {
+
+        time += Time.deltaTime;
+
+        if (time >= interpolationPeriod)
+        {
+            time = 0.0f;
+            Vector3 Position = new Vector3(Random.Range(minX, maxX), yPos);
+            // execute block of code here
+            GameObject obj = Instantiate(Platform, Position, Quaternion.identity);
+
+            Destroy(obj, 5);
+        }
+    }
+}
---
+++ Greek_Dev/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    [SerializeField] float PlayerSpeed;
+    [SerializeField] float minX, maxX;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float movementX = Input.GetAxisRaw("Horizontal");
+
+        transform.position += new Vector3(movementX * PlayerSpeed * Time.deltaTime, 0);
+
+        if (transform.position.x > maxX)
+        {
+            transform.position = new Vector3(maxX, transform.position.y);
+        }
+
+
+        if (transform.position.x < minX)
+        {
+            transform.position = new Vector3(minX, transform.position.y);
+        }
+    }
+}
