3d8423d6cb0a43fa605cfb448c6c9c145ed78d9e
webzerot <webzerot@gmail.com>
2022-06-02 15:40:13 +0300

    Idk

--- Greek_Dev/Assets/Scripts/PlayerMovement.cs
+++ Greek_Dev/Assets/Scripts/PlayerMovement.cs
@@ -7,12 +7,27 @@ public class PlayerMovement : MonoBehaviour
 
     [SerializeField] float PlayerSpeed;
     [SerializeField] float minX, maxX;
+    [SerializeField] float JumpForce = 10;
+    [SerializeField] string PlatformTag = "Platform";
+
+
+    void Start()
+    {
+        Jump();
+    }
+
+    //private bool isGrounded;
     // Update is called once per frame
     void Update()
     {
         float movementX = Input.GetAxisRaw("Horizontal");
 
         transform.position += new Vector3(movementX * PlayerSpeed * Time.deltaTime, 0);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
 
         if (transform.position.x > maxX)
         {
@@ -24,5 +39,25 @@ public class PlayerMovement : MonoBehaviour
         {
             transform.position = new Vector3(minX, transform.position.y);
         }
+
+        if (transform.position.y < -4.5f)
+        {
+            transform.position = new Vector3(transform.position.x, -4.5f);
+        }
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag(PlatformTag))
+        {
+            Jump();
+        }
+    }
+
+
+    void Jump()
+    {
+        GetComponent<Rigidbody2D>().AddForce(Vector2.up * JumpForce, ForceMode2D.Impulse);
     }
 }
